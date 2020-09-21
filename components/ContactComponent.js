a0c4a0c39e9b780f46b0514b8ce0e35e034ffb0e
grgp <grgp722@gmail.com>
2020-09-21 13:45:00 +0300

    sending email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { ScrollView, Text, StyleSheet } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable'; 
+// import { MailComposer } from 'expo'; 
+import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends React.Component { // even though we don't need constr with state here, still had to implement this as a class - to be able to put static navigationOptions in it!
 
   static navigationOptions = { 
     title: 'Contact Us' 
   };
+
+  sendMail() {
+    MailComposer.composeAsync({
+      recepients: ['confusion@food.net'], // array of recepients
+      subject: 'Enquiry',
+      body: 'To whom it may concern:'
+    });
+  }
 
   render() {
     return (
@@ -28,6 +38,13 @@ class Contact extends React.Component { // even though we don't need constr with
                     Email:confusion@food.net
                 </Text>
             </Text> 
+
+            <Button // this button is from react-native elements, and this is an enhanced button than the button that we use from react-native. This allows me to even specify the icon to be used in the button
+              title='  Send Email'
+              buttonStyle={{ backgroundColor: '#512DA8', marginTop: 30 }}
+              icon={<Icon type='font-awesome' name='envelope-o' color='white' />}
+              onPress={this.sendMail}
+            />
           </Card>
 
         </Animatable.View>
